1e5d988331ee0674ed83b883cf858b02b2f48953
Thomas Bosch <thomas.bosch@codecentric.de>
2019-02-05 13:34:59 +0100

    Add basic authentication header if system property is set

--- api/src/main/java/org/sonarsource/scanner/api/internal/OkHttpClientFactory.java
+++ api/src/main/java/org/sonarsource/scanner/api/internal/OkHttpClientFactory.java
@@ -19,6 +19,8 @@
  */
 package org.sonarsource.scanner.api.internal;
 
+import static java.util.Arrays.asList;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -30,6 +32,7 @@ import java.security.NoSuchProviderException;
 import java.security.cert.CertificateException;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
@@ -37,153 +40,166 @@ import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
+
+import org.sonarsource.scanner.api.internal.cache.Logger;
+
 import okhttp3.ConnectionSpec;
 import okhttp3.Credentials;
 import okhttp3.OkHttpClient;
 import okhttp3.internal.tls.OkHostnameVerifier;
-import org.sonarsource.scanner.api.internal.cache.Logger;
-
-import static java.util.Arrays.asList;
 
 public class OkHttpClientFactory {
 
-  static final int CONNECT_TIMEOUT_MILLISECONDS = 5_000;
-  static final int READ_TIMEOUT_MILLISECONDS = 500_000;
-  static final String NONE = "NONE";
-  static final String P11KEYSTORE = "PKCS11";
-  private static final String PROXY_AUTHORIZATION = "Proxy-Authorization";
+	static final int CONNECT_TIMEOUT_MILLISECONDS = 5_000;
 
-  private OkHttpClientFactory() {
-    // only statics
-  }
+	static final int READ_TIMEOUT_MILLISECONDS = 500_000;
 
-  static OkHttpClient create(Logger logger) {
-    OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+	static final String NONE = "NONE";
 
-    okHttpClientBuilder.connectTimeout(CONNECT_TIMEOUT_MILLISECONDS, TimeUnit.MILLISECONDS);
-    okHttpClientBuilder.readTimeout(READ_TIMEOUT_MILLISECONDS, TimeUnit.MILLISECONDS);
+	static final String P11KEYSTORE = "PKCS11";
 
-    ConnectionSpec tls = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-      .allEnabledTlsVersions()
-      .allEnabledCipherSuites()
-      .supportsTlsExtensions(true)
-      .build();
-    okHttpClientBuilder.connectionSpecs(asList(tls, ConnectionSpec.CLEARTEXT));
+	private static final String PROXY_AUTHORIZATION = "Proxy-Authorization";
 
-    X509TrustManager systemDefaultTrustManager = systemDefaultTrustManager();
-    okHttpClientBuilder.sslSocketFactory(systemDefaultSslSocketFactory(systemDefaultTrustManager, logger), systemDefaultTrustManager);
-    okHttpClientBuilder.hostnameVerifier(OkHostnameVerifier.INSTANCE);
+	private static final String AUTHORIZATION = "Authorization";
 
-    // OkHttp detect 'http.proxyHost' java property, but credentials should be filled
-    final String proxyUser = System.getProperty("http.proxyUser", "");
-    if (!System.getProperty("http.proxyHost", "").isEmpty() && !proxyUser.isEmpty()) {
-      okHttpClientBuilder.proxyAuthenticator((route, response) -> {
-        if (response.request().header(PROXY_AUTHORIZATION) != null) {
-          // Give up, we've already attempted to authenticate.
-          return null;
-        }
-        if (HttpURLConnection.HTTP_PROXY_AUTH == response.code()) {
-          String credential = Credentials.basic(proxyUser, System.getProperty("http.proxyPassword"));
-          return response.request().newBuilder().header(PROXY_AUTHORIZATION, credential).build();
-        }
-        return null;
-      });
-    }
-    return okHttpClientBuilder.build();
-  }
+	private OkHttpClientFactory() {
+		// only statics
+	}
 
-  private static X509TrustManager systemDefaultTrustManager() {
-    try {
-      TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-      trustManagerFactory.init((KeyStore) null);
-      TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-      if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-        throw new IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers));
-      }
-      return (X509TrustManager) trustManagers[0];
-    } catch (GeneralSecurityException e) {
-      // The system has no TLS. Just give up.
-      throw new AssertionError(e);
-    }
-  }
+	static OkHttpClient create(Logger logger) {
+		OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
 
-  private static SSLSocketFactory systemDefaultSslSocketFactory(X509TrustManager trustManager, Logger logger) {
-    KeyManager[] defaultKeyManager = getDefaultKeyManager(logger);
-    try {
-      SSLContext sslContext = SSLContext.getInstance("TLS");
-      sslContext.init(defaultKeyManager, new TrustManager[] {trustManager}, null);
-      return sslContext.getSocketFactory();
-    } catch (GeneralSecurityException e) {
-      // The system has no TLS. Just give up.
-      throw new AssertionError(e);
-    }
-  }
+		okHttpClientBuilder.connectTimeout(CONNECT_TIMEOUT_MILLISECONDS, TimeUnit.MILLISECONDS);
+		okHttpClientBuilder.readTimeout(READ_TIMEOUT_MILLISECONDS, TimeUnit.MILLISECONDS);
 
-  /**
-   * Inspired from sun.security.ssl.SSLContextImpl#getDefaultKeyManager()
-   */
-  private static synchronized KeyManager[] getDefaultKeyManager(Logger logger) {
+		ConnectionSpec tls = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS).allEnabledTlsVersions()
+				.allEnabledCipherSuites().supportsTlsExtensions(true).build();
+		okHttpClientBuilder.connectionSpecs(asList(tls, ConnectionSpec.CLEARTEXT));
 
-    final String defaultKeyStore = System.getProperty("javax.net.ssl.keyStore", "");
-    String defaultKeyStoreType = System.getProperty("javax.net.ssl.keyStoreType", KeyStore.getDefaultType());
-    String defaultKeyStoreProvider = System.getProperty("javax.net.ssl.keyStoreProvider", "");
+		X509TrustManager systemDefaultTrustManager = systemDefaultTrustManager();
+		okHttpClientBuilder.sslSocketFactory(systemDefaultSslSocketFactory(systemDefaultTrustManager, logger),
+				systemDefaultTrustManager);
+		okHttpClientBuilder.hostnameVerifier(OkHostnameVerifier.INSTANCE);
 
-    logger.debug("keyStore is : " + defaultKeyStore);
-    logger.debug("keyStore type is : " + defaultKeyStoreType);
-    logger.debug("keyStore provider is : " + defaultKeyStoreProvider);
+		// OkHttp detect 'http.proxyHost' java property, but credentials should be filled
+		final String proxyUser = System.getProperty("http.proxyUser", "");
+		if (!System.getProperty("http.proxyHost", "").isEmpty() && !proxyUser.isEmpty()) {
+			okHttpClientBuilder.proxyAuthenticator((route, response) -> {
+				if (response.request().header(PROXY_AUTHORIZATION) != null) {
+					// Give up, we've already attempted to authenticate.
+					return null;
+				}
+				if (HttpURLConnection.HTTP_PROXY_AUTH == response.code()) {
+					String credential = Credentials.basic(proxyUser, System.getProperty("http.proxyPassword"));
+					return response.request().newBuilder().header(PROXY_AUTHORIZATION, credential).build();
+				}
+				return null;
+			});
+		}
+		if (!System.getProperty("http.basicAuthUser", "").isEmpty()) {
+			String credential = Credentials.basic(System.getProperty("http.basicAuthUser"),
+					System.getProperty("http.basicAuthPassword"));
+			okHttpClientBuilder.authenticator((route, response) -> {
+				return response.request().newBuilder().header(AUTHORIZATION, credential).build();
+			});
+		}
+		return okHttpClientBuilder.build();
+	}
 
-    if (P11KEYSTORE.equals(defaultKeyStoreType) && !NONE.equals(defaultKeyStore)) {
-      throw new IllegalArgumentException("if keyStoreType is " + P11KEYSTORE + ", then keyStore must be " + NONE);
-    }
+	private static X509TrustManager systemDefaultTrustManager() {
+		try {
+			TrustManagerFactory trustManagerFactory = TrustManagerFactory
+					.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+			trustManagerFactory.init((KeyStore) null);
+			TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+			if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+				throw new IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers));
+			}
+			return (X509TrustManager) trustManagers[0];
+		} catch (GeneralSecurityException e) {
+			// The system has no TLS. Just give up.
+			throw new AssertionError(e);
+		}
+	}
 
-    String defaultKeyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword", "");
-    char[] passwd = defaultKeyStorePassword.isEmpty() ? null : defaultKeyStorePassword.toCharArray();
+	private static SSLSocketFactory systemDefaultSslSocketFactory(X509TrustManager trustManager, Logger logger) {
+		KeyManager[] defaultKeyManager = getDefaultKeyManager(logger);
+		try {
+			SSLContext sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(defaultKeyManager, new TrustManager[] { trustManager }, null);
+			return sslContext.getSocketFactory();
+		} catch (GeneralSecurityException e) {
+			// The system has no TLS. Just give up.
+			throw new AssertionError(e);
+		}
+	}
 
-    try {
-      /*
-       * Try to initialize key store.
-       */
-      KeyStore ks = initKeyStore(logger, defaultKeyStore, defaultKeyStoreType, defaultKeyStoreProvider, passwd);
+	/**
+	 * Inspired from sun.security.ssl.SSLContextImpl#getDefaultKeyManager()
+	 */
+	private static synchronized KeyManager[] getDefaultKeyManager(Logger logger) {
 
-      /*
-       * Try to initialize key manager.
-       */
-      logger.debug("init keymanager of type " + KeyManagerFactory.getDefaultAlgorithm());
-      KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		final String defaultKeyStore = System.getProperty("javax.net.ssl.keyStore", "");
+		String defaultKeyStoreType = System.getProperty("javax.net.ssl.keyStoreType", KeyStore.getDefaultType());
+		String defaultKeyStoreProvider = System.getProperty("javax.net.ssl.keyStoreProvider", "");
 
-      // FIXME : The password for opening keys inside the JKS is not mandatory to be the same
-      // as opening the JKS KeyStore
-      if (P11KEYSTORE.equals(defaultKeyStoreType)) {
-        // do not pass key passwd if using token
-        kmf.init(ks, null);
-      } else {
-        kmf.init(ks, passwd);
-      }
+		logger.debug("keyStore is : " + defaultKeyStore);
+		logger.debug("keyStore type is : " + defaultKeyStoreType);
+		logger.debug("keyStore provider is : " + defaultKeyStoreProvider);
 
-      return kmf.getKeyManagers();
-    } catch (Exception e) {
-      throw new IllegalStateException("Unable to initialize key manager", e);
-    }
-  }
+		if (P11KEYSTORE.equals(defaultKeyStoreType) && !NONE.equals(defaultKeyStore)) {
+			throw new IllegalArgumentException("if keyStoreType is " + P11KEYSTORE + ", then keyStore must be " + NONE);
+		}
 
-  private static KeyStore initKeyStore(Logger logger, final String defaultKeyStore, String defaultKeyStoreType, String defaultKeyStoreProvider, char[] passwd)
-    throws KeyStoreException, NoSuchProviderException, IOException, NoSuchAlgorithmException, CertificateException {
-    KeyStore ks = null;
-    if (!defaultKeyStoreType.isEmpty()) {
-      logger.debug("init keystore");
-      if (defaultKeyStoreProvider.isEmpty()) {
-        ks = KeyStore.getInstance(defaultKeyStoreType);
-      } else {
-        ks = KeyStore.getInstance(defaultKeyStoreType, defaultKeyStoreProvider);
-      }
-      if (!defaultKeyStore.isEmpty() && !NONE.equals(defaultKeyStore)) {
-        try (FileInputStream fs = new FileInputStream(defaultKeyStore)) {
-          ks.load(fs, passwd);
-        }
-      } else {
-        ks.load(null, passwd);
-      }
-    }
-    return ks;
-  }
+		String defaultKeyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword", "");
+		char[] passwd = defaultKeyStorePassword.isEmpty() ? null : defaultKeyStorePassword.toCharArray();
+
+		try {
+			/*
+			 * Try to initialize key store.
+			 */
+			KeyStore ks = initKeyStore(logger, defaultKeyStore, defaultKeyStoreType, defaultKeyStoreProvider, passwd);
+
+			/*
+			 * Try to initialize key manager.
+			 */
+			logger.debug("init keymanager of type " + KeyManagerFactory.getDefaultAlgorithm());
+			KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+
+			// FIXME : The password for opening keys inside the JKS is not mandatory to be the same
+			// as opening the JKS KeyStore
+			if (P11KEYSTORE.equals(defaultKeyStoreType)) {
+				// do not pass key passwd if using token
+				kmf.init(ks, null);
+			} else {
+				kmf.init(ks, passwd);
+			}
+
+			return kmf.getKeyManagers();
+		} catch (Exception e) {
+			throw new IllegalStateException("Unable to initialize key manager", e);
+		}
+	}
+
+	private static KeyStore initKeyStore(Logger logger, final String defaultKeyStore, String defaultKeyStoreType,
+			String defaultKeyStoreProvider, char[] passwd) throws KeyStoreException, NoSuchProviderException,
+			IOException, NoSuchAlgorithmException, CertificateException {
+		KeyStore ks = null;
+		if (!defaultKeyStoreType.isEmpty()) {
+			logger.debug("init keystore");
+			if (defaultKeyStoreProvider.isEmpty()) {
+				ks = KeyStore.getInstance(defaultKeyStoreType);
+			} else {
+				ks = KeyStore.getInstance(defaultKeyStoreType, defaultKeyStoreProvider);
+			}
+			if (!defaultKeyStore.isEmpty() && !NONE.equals(defaultKeyStore)) {
+				try (FileInputStream fs = new FileInputStream(defaultKeyStore)) {
+					ks.load(fs, passwd);
+				}
+			} else {
+				ks.load(null, passwd);
+			}
+		}
+		return ks;
+	}
 }
